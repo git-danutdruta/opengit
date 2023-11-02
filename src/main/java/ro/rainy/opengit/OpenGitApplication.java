a51484ee342e4f51a598d2f55f8e3aff412b9d6e
Daniel <danutdruta@gmail.com>
2023-11-02 12:04:09 +0200

    oops, forgot to add the file

--- src/main/java/ro/rainy/opengit/OpenGitApplication.java
+++ src/main/java/ro/rainy/opengit/OpenGitApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class OpenGitApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(OpenGitApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(OpenGitApplication.class.getResource("opengit-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
