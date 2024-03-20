4f3d15efc62fab4779342f11b476f334a488d543
Daniel <danutdruta@gmail.com>
2024-03-21 00:16:22 +0200

    OpenGitView - buttons alignment

--- src/main/java/ro/rainy/opengit/OpenGitApplication.java
+++ src/main/java/ro/rainy/opengit/OpenGitApplication.java
@@ -9,13 +9,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import ro.rainy.opengit.component.AppInfo;
+import ro.rainy.opengit.view.OpenGitView;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.Objects;
 
 @SpringBootApplication
 public class OpenGitApplication extends Application {
-    public static final String OPENGIT_VIEW_FXML = "view/opengit-view.fxml";
+    private static final String OPENGIT_VIEW_FXML = "view/opengit-view.fxml";
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -23,8 +24,9 @@ public class OpenGitApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(OpenGitApplication.class.getResource(OPENGIT_VIEW_FXML));
 
         AppInfo appInfo = context.getBean(AppInfo.class);
+        OpenGitView gitView = context.getBean(OpenGitView.class);
 
-        Scene scene = new Scene(fxmlLoader.load(), appInfo.getWidth(), appInfo.getHeight());
+        Scene scene = new Scene(gitView.getTopLayer(), appInfo.getWidth(), appInfo.getHeight());
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("style/opengit.css")).toExternalForm());
 
         stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResource("img/opengit_logo.png")).toExternalForm()));
@@ -37,5 +39,36 @@ public class OpenGitApplication extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+}
+
+class Other {
+
+    public static void main(String[] args) {
+//        ProcessBuilder pb = new ProcessBuilder("git ","fetch", "origin");
+        ProcessBuilder pb = new ProcessBuilder("git ","log","--all","--decorate", "--oneline","--graph");
+        pb.directory(new File("E:\\Workspace\\opengit"));
+        try {
+            Process start = pb.start();
+            InputStream is = start.getInputStream();
+            LineNumberReader lnr = new LineNumberReader(new InputStreamReader(is));
+            String line;
+            while((line = lnr.readLine())!= null){
+                System.out.println(line);
+            }
+            lnr.close();
+            is.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+class Test{
+    public static void main(String[] args) {
+         int a = 0x000F;
+         int b = 0x2222;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a&b);
     }
 }
